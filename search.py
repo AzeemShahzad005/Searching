a09b37089a7b67a6658d4937810d2a0a901847b1
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2020-02-27 23:56:25 +0500

    Update search.py

--- search.py
+++ search.py
@@ -3,5 +3,5 @@ random_string = "This is my random string"
  # Check whether 'in' exists in random_string
 print ('is' in random_string)
 
-# 'random' exists!
+
 print ('random' in random_string) # 'random' exists!
